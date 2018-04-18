4cdc279edc2318c9a8d140ca3695b37c1e8d3724
Rohan <rrohan2697@gmail.com>
2018-04-18 15:52:51 +0530

    initial update
    all files

---
+++ abc.js
@@ -0,0 +1,36 @@
+
+    $(document).ready(function(e){
+        
+        $('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+			width:$(this).attr('data-percent')
+		},6000);
+	});
+	
+	$('.skillbar').each(function(){
+		$(this).find('.skill-bar-percent').animate({
+			left:$(this).attr('data-percent')
+		},6000);
+	});
+	
+	$('.skillbar').each(function(){
+		$(this).find('.skill-bar-percent').text($(this).attr('data-percent'));
+	});
+     $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+        $('.go-top').fadeIn(2000);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      }) 
+
+
+        
+        });
+    
+    
